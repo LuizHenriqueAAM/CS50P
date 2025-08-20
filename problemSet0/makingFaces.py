83041da1efa9636cc1da8be26c1b5a2b41e7de02
LuizHenriqueAAM <Alvesluizhenrique3@gmail.com>
2025-08-20 09:46:27 -0300

    Making faces fixed and completed

--- problemSet0/makingFaces.py
+++ problemSet0/makingFaces.py
@@ -1,11 +1,12 @@
 def main():
-    text = input("Write something and put a :) or a :( on it")
+    text = input("Write something and put a :) or a :( on it: ")
     convert(text)
 
 def convert(text:str):
-    text.replace(":)", "🙂")
-    text.replace(":(", "🙁")
-    print(f"{text}")
+    newText:str
+    newText = text.replace(":)", "🙂")
+    newText = newText.replace(":(", "🙁")
+    print(f"{newText}")
 
 
 main()
