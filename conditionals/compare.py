b39f32a1bf0ef04a37295aee89486637fb0315ab
LuizHenriqueAAM <Alvesluizhenrique3@gmail.com>
2025-08-21 15:37:05 -0300

    Lerning how to use the conditionals: if, elif and else

---
+++ conditionals/compare.py
@@ -0,0 +1,14 @@
+x:int
+y:int
+
+x = int(input("What's X? "))
+y = int(input("What's Y? "))
+
+if x < y:
+    print("X is less than Y")
+
+elif x > y:
+    print("X is greater than Y")
+
+else:
+    print("X is equal to Y")
